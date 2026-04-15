2250f99c3c19874932b7e9497d7ebdac11277aed
aHilsberg <hilsberg.anton@gmail.com>
2026-04-15 22:57:19 +0200

    feat: add formating git hook

--- modules/git-hooks.nix
+++ modules/git-hooks.nix
@@ -1,15 +1,25 @@
 {
+  flake-parts-lib,
   lib,
   inputs,
   ...
 }: {
-  # imports = [
-  #   inputs.git-hooks-nix.flakeModule
-  # ];
+  imports = [
+    inputs.git-hooks-nix.flakeModule
+  ];
 
-  # perSystem = { config, ... }: {
-  #   pre-commit.settings.hooks = {
-  #     nixpkgs-fmt.enable = lib.mkDefault true;
-  #   };
-  # };
+  options.perSystem = flake-parts-lib.mkPerSystemOption ({config, ...}: {
+    options.git-hooks = {
+      enable = lib.mkEnableOption "Git hooks for this project";
+    };
+
+    config = lib.mkIf (config.git-hooks.enable && config.formatting.enable) {
+      pre-commit.settings.hooks = {
+        treefmt = {
+          enable = true;
+          packageOverrides.treefmt = config.treefmt.build.wrapper;
+        };
+      };
+    };
+  });
 }
